525f737d6300095eac718e641e55057b23570199
chIorophyII <dab2in79@gmail.com>
2022-04-29 04:51:09 +0900

    [#11]feat kakaologin

---
+++ src/main/java/com/sparta/startup_be/dto/GoogleUserInfoDto.java
@@ -0,0 +1,11 @@
+//package com.sparta.startup_be.dto;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@NoArgsConstructor
+//public class GoogleUserInfoDto {
+//    private
+//
+//}

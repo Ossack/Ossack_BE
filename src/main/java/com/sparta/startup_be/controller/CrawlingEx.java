2b86fecb695f277bcc9cf356e33e345141dc11dd
LIttlecold4 <minu09252@gmail.com>
2022-05-02 22:35:48 +0900

    naver_crawling2

--- src/main/java/com/sparta/startup_be/controller/CrawlingEx.java
+++ src/main/java/com/sparta/startup_be/controller/CrawlingEx.java
@@ -12,6 +12,6 @@ public class CrawlingEx {
     @GetMapping("/api/ex")
     public void example() throws InterruptedException {
         WebDriverUtil webDriverUtil = new WebDriverUtil();
-        webDriverUtil.useDriver("https://m.land.naver.com/map/37.5612177:126.9597597:14:/SMS/A1:B1:B2:B3#mapFullList");
+        webDriverUtil.useDriver("https://m.land.naver.com/map/37.5571885:126.9188185:18:/SMS/A1:B1:B2:B3#mapFullList");
     }
 }
--- src/main/java/com/sparta/startup_be/utils/WebDriverUtil.java
+++ src/main/java/com/sparta/startup_be/utils/WebDriverUtil.java
@@ -54,7 +54,7 @@ public class WebDriverUtil {
             e.printStackTrace();
         }
         WebElement item = driver.findElement(By.className("article_box"));
-        int m = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"_listContainer\"]/div/div[1]/a/h3/strong")).getText());
+        int m = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"_listContainer\"]/div/div[1]/a/h3/strong")).getText().replace("+",""));
         int j=0;
         while(true) {
 
@@ -64,18 +64,47 @@ public class WebDriverUtil {
             if(j==m) break;
         }
 
+//        int i=0;
 
         List<WebElement> webElements = driver.findElements(By.className("item_area"));
+//        for(WebElement webElement: webElements){
+//            try{
+//                System.out.println(i++);
+//                System.out.println(webElement.findElement(By.className("title_place")).getText()); //중소형 사무실
+//                System.out.println(webElement.findElement(By.className("price_area")).getText());  //월세
+//                System.out.println(webElement.findElement(By.className("information_area")).getText()); //기본정보
+//                System.out.println(webElement.findElement(By.className("spec")).getText()); //스펙
+//                System.out.println(webElement.findElement(By.className("agent_name")).getText()); //부동산
+//            }catch(Exception e) {
+//                System.out.println("\n\n\n\n\n뭐하나 없당\n\n\n\n\n");
+//            }
+//        }
+
+        System.out.println(driver.findElement(By.tagName("head")).getText());
 
         int i=0;
         for(WebElement webElement : webElements){
-            System.out.println(i);
-            System.out.println("타입:"+webElement.findElement(By.className("type")).getText());
-            System.out.println("매물이름:"+webElement.findElement(By.className("title_place")).getText()+webElement.findElement(By.className("title_building")).getText());
-            System.out.println("가격:"+webElement.findElement(By.className("price")).getText());
-            System.out.println("정보:"+webElement.findElement(By.className("info")).getText());
-            System.out.println("부동산이름:"+webElement.findElement(By.className("type")).getText());
-            i++;
+            System.out.println(webElement.findElement(By.className("merit_area")).getText());
+            if(!webElement.findElement(By.className("merit_area")).getText().contains("중개사")){
+                webElement.click();
+                Thread.sleep(1000);
+                i++;
+                System.out.println("i="+i);
+                System.out.println("body"+driver.findElement(By.tagName("head")).getText());
+                System.out.println(driver.findElement(By.className("detail_deal_kind")).getText());
+                System.out.println("매물:"+driver.findElement(By.tagName("div")).getText());
+//                System.out.println("타입:"+driver.findElement(By.xpath("//*[@id=\"detailMy--fixed\"]/em")).getText());
+//                System.out.println("가격:"+driver.findElement(By.xpath("//*[@id=\"detailMy--fixed\"]/strong")).getText());
+                driver.navigate().back();
+            }
+//            System.out.println("정보:"+webElement.findElement(By.className("detail_introduction_text")).getText());
+//            List<WebElement> images = driver.findElements(By.className("detail_photo_inner"));
+//            List<WebElement> infos = driver.findElements(By.className("detail_table_row"));
+//            for(WebElement info: infos){
+//                System.out.println(info.getText());
+//            }
+//            System.out.println("주소:"+webElement.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div[6]/div[2]/em")).getText());
+//            System.out.println("부동산이름:"+webElement.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div[7]/div[1]/h3")).getText());
         }
         log.info("++++++++++++++++++++++===================+++++++++++++ 끝 : " );
 

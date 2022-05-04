08f2df3265d03317110854559de6b6c53fe60dae
sumye <wltnals00@naver.com>
2022-05-04 13:17:57 +0900

    refactor 전체적인 방향 수정 및 완료

--- src/main/java/com/sparta/startup_be/repository/FavoriteRepository.java
+++ src/main/java/com/sparta/startup_be/repository/FavoriteRepository.java
@@ -3,19 +3,13 @@ package com.sparta.startup_be.repository;
 
 import com.sparta.startup_be.model.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-
-//    @Query("SELECT COUNT(favoriteId) FROM Favorite WHERE post.postId = :postId")
-//    Long findByCountByPost(@Param("postId") Long postId);
-//
-    Optional<Favorite> findByPost_PostIdAndUser_Nickname(Long postId, String nickname);
-
-    void deleteAllByPost_PostId(Long postId);
-
-    Optional<Favorite> findByUser_UserEmailAndPost_PostId(String userEmail, Long postId);
+    Optional<Favorite> findByUserid(Long userid);
+    Optional<Favorite> findByUseridAndEstateid(Long userid, Long estateid);
+    void deleteByUseridAndEstateid(Long userid, Long estateid);
+    List<Favorite> findAllByEstateid(Long estateid);
 }
--- src/main/java/com/sparta/startup_be/service/FavoriteService.java
+++ src/main/java/com/sparta/startup_be/service/FavoriteService.java
@@ -1,49 +1,38 @@
 package com.sparta.startup_be.service;
 
 
-import com.sparta.startup_be.dto.FavoriteResponseDto;
-import com.sparta.startup_be.model.Estate;
+import com.sparta.startup_be.dto.FavoriteDto;
 import com.sparta.startup_be.model.Favorite;
-import com.sparta.startup_be.model.User;
-import com.sparta.startup_be.repository.EstateRepository;
 import com.sparta.startup_be.repository.FavoriteRepository;
-import com.sparta.startup_be.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+
 
 @RequiredArgsConstructor
 @Service
 public class FavoriteService {
+    private final FavoriteRepository favoriteRepository;
 
-    private  final FavoriteRepository favoriteRepository;
-    private final UserRepository userRepository;
-    private final EstateRepository estateRepository;
+    public void pressLike(Long estateid, Long userid){
+        Optional<Favorite> favorite = favoriteRepository.findByUseridAndEstateid(userid,estateid);
+        if(favorite.isPresent()){
+            throw new IllegalArgumentException("이미 눌러진 좋아요 입니다");
+        }
+        FavoriteDto favoriteDto = new FavoriteDto(userid,estateid);
+        Favorite favorite1 = new Favorite(favoriteDto);
+        favoriteRepository.save(favorite1);
+    }
 
-    // 찜하기 기능
+
     @Transactional
-    public FavoriteResponseDto favoriteCheck(Long postId, String userEmail) {
-
-        Estate estate = estateRepository.findById(postId).orElse(null);
-        if(estate == null){
-            return  new FavoriteResponseDto(400,"존재하지 않는 게시글 입니다");
-        }
-
-        User user = userRepository.findByUserEmail(userEmail).orElse(null);
-        if(user == null){
-            return  new FavoriteResponseDto(400,"존재하지 않는 회원입니다.");
-        }
-
-        Favorite favorite = favoriteRepository.findByUser_UserEmailAndPost_PostId(userEmail,postId).orElse(null);
-
-        if(favorite == null){
-            Favorite saveFavorite = new Favorite(estate,user);
-            favoriteRepository.save(saveFavorite);
-            return new FavoriteResponseDto(200,null, true );
-        } else {
-            favoriteRepository.deleteById(favorite.getFavoriteId());
-            return new FavoriteResponseDto(200,null, false);
-        }
+    public void unpressLike(Long estateid,Long userid){
+        Favorite favorite = favoriteRepository.findByUseridAndEstateid(userid,estateid).orElseThrow(
+                ()-> new IllegalArgumentException("이미 좋아요가 취소되었습니다")
+        );
+        favoriteRepository.deleteByUseridAndEstateid(userid, estateid);
     }
 }
+

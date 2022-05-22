ae87988094b297125d7a1dfb2844e891d24efd51
chIorophyII <dab2in79@gmail.com>
2022-05-23 02:30:24 +0900

    [#94] refactor CI/CD

---
+++ src/main/java/com/sparta/startup_be/cicd/CicdController.java
@@ -0,0 +1,24 @@
+package com.sparta.startup_be.cicd;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class CicdController {
+    @Value("${ossack-module.version}")
+    private String version;
+
+    @GetMapping("/")
+    public String version() {
+        return String.format("Project Version : %s", version);
+    }
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+
+}

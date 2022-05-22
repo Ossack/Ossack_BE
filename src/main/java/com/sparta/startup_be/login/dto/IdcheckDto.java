c135117b13c929c76979df97a1f7b07d1bd5d0bb
chIorophyII <dab2in79@gmail.com>
2022-05-22 15:53:12 +0900

    [#92] fix 이메일 중복확인 유효성 검사

---
+++ src/main/java/com/sparta/startup_be/login/dto/IdcheckDto.java
@@ -0,0 +1,15 @@
+package com.sparta.startup_be.login.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@NoArgsConstructor
+public class IdcheckDto {
+    @NotBlank
+    @Pattern(regexp = "([\\w-.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([\\w-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$", message = "이메일 형식이 맞지 않습니다.")
+    private String userEmail;
+}
